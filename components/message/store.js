02343d167efe0ee9882e58917cb5a63341d3cc08
georgetegral <georgetegral@gmail.com>
2020-04-05 21:07:16 -0500

    created model for uploading data to mongodb

--- components/message/store.js
+++ components/message/store.js
@@ -1,13 +1,26 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
 
-function addMessage(message)
-{
-    list.push(message);
+db.Promise = global.Promise;
+
+db.connect('mongodb+srv://db_user_node:db_user_node@nodetest-6arkr.azure.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'telegram'
+});
+
+//console.log('[db] conectada con exito');
+
+function addMessage(message) {
+    // list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessages()
-{
-    return list;
+async function getMessages() {
+    // return list;
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
