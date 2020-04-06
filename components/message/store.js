8e11022e63e947414405dae4eeb4b9ef08b7c134
georgetegral <georgetegral@gmail.com>
2020-04-05 22:43:27 -0500

    created db.js for managing mongodb connection

--- components/message/store.js
+++ components/message/store.js
@@ -1,15 +1,4 @@
-const db = require('mongoose');
 const Model = require('./model');
-
-db.Promise = global.Promise;
-
-db.connect('mongodb+srv://db_user_node:db_user_node@nodetest-6arkr.azure.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: 'telegram'
-});
-
-//console.log('[db] conectada con exito');
 
 function addMessage(message) {
     const myMessage = new Model(message);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./db');
 const router = require('./network/routes');
+
+db('mongodb+srv://db_user_node:db_user_node@nodetest-6arkr.azure.mongodb.net/test?retryWrites=true&w=majority')
 
 var app = express();
 app.use(bodyParser.json());
